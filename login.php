2c7d35b47a26df321c40f6add61cdea5fd1ca41a
JordanWheelerDev <163913618+JordanWheelerDev@users.noreply.github.com>
2024-07-14 20:22:29 -0230

    Favicon update

--- login.php
+++ login.php
@@ -16,6 +16,7 @@ if (isset($_POST['loginBtn'])) {
 <html lang="en">
 
 <head>
+    <link rel="icon" type="image/png" href="https://dischub.org/images/logo-w.png">
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
--- servers.php
+++ servers.php
@@ -30,6 +30,7 @@ while ($row = $result->fetch_assoc()) {
 <html lang="en">
 
 <head>
+    <link rel="icon" type="image/png" href="https://dischub.org/images/logo-w.png">
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
