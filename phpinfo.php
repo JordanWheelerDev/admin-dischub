1c68be2b804176809c5a3894ef282519ae6db206
JordanWheelerDev <163913618+JordanWheelerDev@users.noreply.github.com>
2024-07-14 20:02:42 -0230

    Update phpinfo.php

--- phpinfo.php
+++ phpinfo.php
@@ -1,2 +1,10 @@
 <?php
-phpinfo();
+$url = "https://discord.com";
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($ch);
+$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+curl_close($ch);
+echo "HTTP Status Code: " . $httpCode . "<br>";
+echo "Response: " . $response;
+?>
